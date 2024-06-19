75d8be027e20a0930e77e678c4d0873b15e22219
leMinhPhuong <phuongleminh841@gmail.com>
2024-06-19 14:49:00 +0700

    finish the basic

--- LinkedList.js
+++ LinkedList.js
@@ -62,19 +62,61 @@ class LinkedList {
     return parent.value;
   }
   at(index) {
-    index = index - 2;
     let parent = this.root;
     let current = this.root;
     while (current != null && index) {
       parent = current;
       current = current.next;
       index--;
-      console.log(index);
     }
-    if (index < 0) {
+    if (index > 0) {
       return false;
     }
     return parent.value;
+  }
+  pop() {
+    let parent = this.root;
+    let current = this.root;
+    while (current.next != null) {
+      parent = current;
+      current = current.next;
+    }
+    parent.next = null;
+    return current;
+  }
+  contains(value) {
+    let result = false;
+    let current = this.root;
+    while (current != null) {
+      if (value == current.value) {
+        result = true;
+      }
+      current = current.next;
+    }
+    return result;
+  }
+  find(value) {
+    let result = 0;
+    let current = this.root;
+    while (current != null) {
+      console.log(result);
+      result++;
+      if (current.value == value) {
+        return result;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+  toString() {
+    let current = this.root;
+    let result = [];
+    while (current != null) {
+      result.push(current.value);
+      current = current.next;
+    }
+    result.push("null");
+    return result;
   }
 }
 const linkedList = new LinkedList();
@@ -88,4 +130,9 @@ linkedList.append(99);
 console.log("the size of linked list is: " + linkedList.size());
 console.log("the head value of linked list:" + linkedList.head());
 console.log("the tail value of linked list:" + linkedList.tail());
-console.log("the value at 5 in the linked list:" + linkedList.at(9));
+console.log("the value at 5 in the linked list:" + linkedList.at(5));
+linkedList.pop();
+console.log("the tail value of linked list:" + linkedList.tail());
+console.log("Linked list contains 1444:" + linkedList.contains(1444));
+console.log("Position of 39:" + linkedList.find(39));
+console.log("Linked list to string is:" + linkedList.toString());
